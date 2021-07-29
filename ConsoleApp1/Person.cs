996243c2112aa38ebef9f45ddd658bee4e0186b9
DCP529 <61003286+DCP529@users.noreply.github.com>
2021-07-29 22:58:05 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Person
+    {
+        string Name { get; set; }
+        decimal Dollar { get; set; }
+        int Id { get; set; }
+
+        public Person(string name, decimal dollar, int id)
+        {
+            Name = name;
+            Dollar = dollar;
+            Id = id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Person result = (Person)obj;
+            if (obj == null)
+            {
+                return false;
+            }
+
+            if (!(obj is Person))
+            {
+                return false;
+            }
+            return result.Name == Name && result.Dollar == Dollar && result.Id == Id;
+        }        
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        public static List<Person> pers = new List<Person>();
+        static void Main(string[] args)
+        {
+            #region Equals
+            //var square1 = new Figure() { SideCount = 4, SideLenght = 1 };
+            //var square2 = new Figure() { SideCount = 4, SideLenght = 1 };
+            //var square3 = square1;
+
+            //bool status1 = square1 == square2;
+            //bool status2 = square1 == square3;
+
+            //bool status3 = square1.Equals(square2);
+            //bool status4 = square1.Equals(square3);
+            #endregion
+
+            //TODO: сделать тоже самое со структурами
+
+            #region List
+            //List<Figure> list = new List<Figure>();
+
+            //list.Add(square1);
+            //list.AddRange(new Figure[] { square1, square2, square3 });
+            //int count = list.Count;
+            //bool result = list.Contains(square3);
+
+            //list.Remove(square2);
+            //list.Clear();
+            #endregion
+
+            Person person1 = new Person("Vova", 15_000, 1);
+            Person person2 = new Person("Kostia", 15_000, 2);
+            Person person3 = new Person("Nikita", 15_000, 4);
+            Person person4 = new Person("Nikita", 15_000, 4);
+
+
+            pers.AddRange(new Person[] { person1, person2 });
+            Adds(person2);
+            Adds(person3);
+
+            bool result = person4.Equals(person2); 
+
+            Console.ReadLine();
+
+            //релизовать хранилище клиентов
+            //имя счет id
+            //при попытке добаления проверить его наличие в списке
+        }
+
+        public static void Adds(Person person)
+        {
+            if (!pers.Contains(person))
+            {
+                pers.Add(person); 
+                Console.WriteLine("Добавление произошло успешно");
+            }
+            else
+            {
+                Console.WriteLine("Такой человек уже есть");
+            }
+        }
+    }
+}
